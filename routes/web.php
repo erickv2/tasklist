e10fabf4bebde27fcb0099f98591a8fc66161b12
gitcore33 <erickestanislau25@gmail.com>
2025-05-11 12:48:43 -0300

    playing with routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'main page';
+});
+
+Route::get('/xxx', function () {
+    return 'hello';
+})->name('hello');
+
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'still got somewhere';
 });
