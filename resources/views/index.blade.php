0dd7932294522200db10e3ecc7a452cf4547e14a
gitcore33 <erickestanislau25@gmail.com>
2025-06-07 14:07:40 -0300

    creates pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,18 @@
 
 @section('content')
 
-    {{-- @if(count($tasks)) --}}
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
 
-        @forelse($tasks as $task)
-            <div>
-                <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{$task->title}}</a>
-            </div>
-        @empty
-            <div>There are no tasks</div>
-        @endforelse
-
-    {{-- @endif --}}
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 
 @endsection
-
